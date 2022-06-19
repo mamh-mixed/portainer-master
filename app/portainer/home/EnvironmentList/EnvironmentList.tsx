0884aa5c242ed89088032d8f46bd9bcad6311f25
Chaim Lev-Ari <chiptus@gmail.com>
2022-06-19 15:27:37 +0300

    refactor(teams): migrate teams to react [EE-2273]
    closes [EE-2273]
    
    refactor(teams): move to react codebase

--- app/portainer/home/EnvironmentList/EnvironmentList.tsx
+++ app/portainer/home/EnvironmentList/EnvironmentList.tsx
@@ -9,7 +9,7 @@ import {
   EnvironmentStatus,
 } from '@/portainer/environments/types';
 import { EnvironmentGroupId } from '@/portainer/environment-groups/types';
-import { useIsAdmin } from '@/portainer/hooks/useUser';
+import { useUser } from '@/portainer/hooks/useUser';
 import {
   HomepageFilter,
   useHomePageFilter,
@@ -72,7 +72,7 @@ const allEnvironmentType = [
 ];
 
 export function EnvironmentList({ onClickItem, onRefresh }: Props) {
-  const isAdmin = useIsAdmin();
+  const { isAdmin } = useUser();
 
   const [platformType, setPlatformType] = useHomePageFilter(
     'platformType',
