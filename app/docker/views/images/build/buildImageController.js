516216a93337309819be686e6bc9ff82c80be088
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2022-10-16 15:09:55 +0300

    refactor(docker/images): use box selector

--- app/docker/views/images/build/buildImageController.js
+++ app/docker/views/images/build/buildImageController.js
@@ -1,7 +1,11 @@
+import { options } from './options';
+
 angular.module('portainer.docker').controller('BuildImageController', BuildImageController);
 
 /* @ngInject */
 function BuildImageController($scope, $async, $window, ModalService, BuildService, Notifications, HttpRequestHelper) {
+  $scope.options = options;
+
   $scope.state = {
     BuildType: 'editor',
     actionInProgress: false,
@@ -28,6 +32,12 @@ function BuildImageController($scope, $async, $window, ModalService, BuildServic
   $scope.$on('$destroy', function () {
     $scope.state.isEditorDirty = false;
   });
+
+  $scope.onChangeBuildType = function (type) {
+    $scope.$evalAsync(() => {
+      $scope.state.BuildType = type;
+    });
+  };
 
   $scope.checkName = function (index) {
     var item = $scope.formValues.ImageNames[index];
